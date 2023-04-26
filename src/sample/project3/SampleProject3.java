afc361446127b8ffea029530c1ace4c3a266a6bf
sewumini <piumisewumini@gmail.com>
2023-04-26 23:38:41 +0530

    Github Java Project

---
+++ src/sample/project3/SampleProject3.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sample.project3;
+
+/**
+ *
+ * @author piumi
+ */
+public class SampleProject3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          Register rg= new Register();
+        rg.setVisible(true);
+        rg.pack();
+        rg.setLocationRelativeTo(null);
+        rg.setDefaultCloseOperation(Register.EXIT_ON_CLOSE);
+    }
+    
+}
